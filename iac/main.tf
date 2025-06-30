880538e8ec29d8fdeeeae8d9f8d4f337faf49d8b
Brian E <57373025+brianelkington@users.noreply.github.com>
2025-06-30 13:37:02 -0600

    clean up extraneous files, add law back into infra

--- iac/main.tf
+++ iac/main.tf
@@ -19,35 +19,35 @@ resource "azurerm_user_assigned_identity" "rkw" {
   tags = local.tags
 }
 
-# resource "azurerm_log_analytics_workspace" "rkw" {
-#   name                = "law-rkw"
-#   location            = azurerm_resource_group.rkw.location
-#   resource_group_name = azurerm_resource_group.rkw.name
+resource "azurerm_log_analytics_workspace" "rkw" {
+  name                = "law-rkw"
+  location            = azurerm_resource_group.rkw.location
+  resource_group_name = azurerm_resource_group.rkw.name
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
 # import {
 #   id = "/subscriptions/${var.subscription_id}/resourceGroups/${azurerm_resource_group.rkw.name}/providers/Microsoft.OperationalInsights/workspaces/law-rkw"
 #   to = azurerm_log_analytics_workspace.rkw
 # }
 
-# resource "azurerm_monitor_diagnostic_setting" "rkw" {
-#   name                       = "diag-cog-to-law"
-#   target_resource_id         = azurerm_cognitive_account.rkw.id
-#   log_analytics_workspace_id = azurerm_log_analytics_workspace.rkw.id
+resource "azurerm_monitor_diagnostic_setting" "rkw" {
+  name                       = "diag-cog-to-law"
+  target_resource_id         = azurerm_cognitive_account.rkw.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.rkw.id
 
-#   # enable all metrics categories
-#   enabled_metric {
-#     category = "AllMetrics"
-#   }
+  # enable all metrics categories
+  enabled_metric {
+    category = "AllMetrics"
+  }
 
-#   # enable the built-in log categories (Audit, Operational, etc)
-#   # adjust categories as needed for your scenario
-#   enabled_log {
-#     category = "allLogs"
-#   }
-#   # enabled_log {
-#   #   category = "AuditEvent"
-#   # }
-# }
+  # enable the built-in log categories (Audit, Operational, etc)
+  # adjust categories as needed for your scenario
+  enabled_log {
+    category = "allLogs"
+  }
+  # enabled_log {
+  #   category = "AuditEvent"
+  # }
+}
